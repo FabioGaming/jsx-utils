c108168a335eb832976f32b43ac92e3c2d08f501
Fabio Gaming <fabio.furch@gmail.com>
2026-05-26 15:58:19 +0200

    feat(Switch): add Switch component for conditional rendering
    - Implemented the Switch component to render children based on Match conditions.
    - Utilizes React's Children API to process child elements.

---
+++ src/components/Switch.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Match, MatchProps } from "src/components/Match";
+
+export function Switch(props: { children: React.ReactNode }) {
+  const children = React.Children.toArray(props.children);
+
+  for (const child of children) {
+    if (!React.isValidElement(child)) continue;
+    if (child.type !== Match) continue;
+    const matchProps = child.props as MatchProps;
+    if (matchProps.when) {
+      return <>{matchProps.children}</>;
+    }
+  }
+
+  return null;
+}
